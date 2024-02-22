48799cefd22659fcd90efa080eab8beae90437da
Pablo Sal <pablo.sal.dejusto@iessanmamede.com>
2024-02-22 10:43:47 +0100

    Inicio Ejercicio

---
+++ src/main/java/ad/apiud6pablo/ApiUd6PabloApplication.java
@@ -0,0 +1,13 @@
+package ad.apiud6pablo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUd6PabloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiUd6PabloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ad/apiud6pablo/ApiUd6PabloApplicationTests.java
@@ -0,0 +1,13 @@
+package ad.apiud6pablo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUd6PabloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
